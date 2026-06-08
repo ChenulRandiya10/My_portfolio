1c308d29ffb1579c4ea8c450302c47279a44b4e6
ChenulRandiya10 <chenulrandiya10@gmail.com>
2026-06-09 02:43:35 +0530

    aaa

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'url';
-import { dirname, resolve } from 'path';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, '../dist'),
-  },
   server: {
     port: 5173,
     proxy: {
