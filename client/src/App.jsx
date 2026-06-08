898d9aaf9b8dd6cc53c551332a79ab322cea3236
ChenulRandiya10 <chenulrandiya10@gmail.com>
2026-06-08 23:37:58 +0530

    deploy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Messages from './pages/Messages';
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <SpeedInsights />
       <div style={{ paddingTop: '92px', scrollPaddingTop: '110px' }}>
         <Routes>
           <Route path="/" element={<Home />} />
+          <SpeedInsights />
           <Route path="/messages" element={<Messages />} />
         </Routes>
       </div>
